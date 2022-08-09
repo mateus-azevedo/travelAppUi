f798bd9f1c99aeed82f0510ae4e1e257d905cd31
Mateus Santos <m.santos.azeve@gmail.com>
2022-08-08 21:55:45 -0300

    feat: utiliza tabNavigate e stackNavigate

--- App.js
+++ App.js
@@ -10,11 +10,26 @@ import Home from './components/Home';
 import Liked from './components/Liked';
 import Profile from './components/Profile';
 
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <TabNavigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Liked" component={Liked} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </TabNavigator>
+  );
+};
+
 const App = () => {
   return (
-    <View>
-      <Text>App.js</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="TabNavigator" component={TabNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
