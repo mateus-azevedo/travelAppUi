994028be2546751c20a8b2f6043bbadbbc22f5ad
Mateus Santos <m.santos.azeve@gmail.com>
2022-08-08 21:15:50 -0300

    style: executa command lint --fix

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Details from './components/Details';
 import Home from './components/Home';
