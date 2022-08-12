5fbad2adf440fbf56017ee91cf9af80d4b9a101f
Mateus Santos <m.santos.azeve@gmail.com>
2022-08-12 14:27:38 -0300

    feat: adiciona estilo ao header

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,57 @@
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { View, StyleSheet, ScrollView, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import Feather from 'react-native-vector-icons/Feather';
+import colors from '../assets/colors/colors';
+import profile from '../assets/images/person.png';
+
+import activitiesData from '../assets/data/activitiesData';
+import discoverCategoriesData from '../assets/data/discoverCategoriesData';
+import discoverData from '../assets/data/discoverData';
+import learnMoreData from '../assets/data/learnMoreData';
+
+Feather.loadFont();
 
 const Home = ({ navigation }) => {
   return (
-    <View>
-      <Text>Home</Text>
-      <Button onPress={() => navigation.navigate('Details')} title="Click Me" />
+    <View style={styles.container}>
+      <ScrollView>
+        {/* Header */}
+        <SafeAreaView>
+          <View style={styles.menuWrapper}>
+            <Feather
+              name="menu"
+              size={32}
+              color={colors.black}
+              style={styles.menuIcon}
+            />
+            <Image source={profile} style={styles.profileImage} />
+          </View>
+        </SafeAreaView>
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: colors.white,
+  },
+  menuWrapper: {
+    marginHorizontal: 20,
+    marginTop: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  menuIcon: {},
+  profileImage: {
+    width: 52,
+    height: 52,
+    borderRadius: 10,
+  },
+});
 
 export default Home;
