d8bcb9cc7f90eb08a62fa609423c98f218b964d6
Mateus Santos <m.santos.azeve@gmail.com>
2022-08-16 16:22:57 -0300

    feat: cria layout do details

--- components/Details.js
+++ components/Details.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 
+import Entypo from 'react-native-vector-icons/Entypo';
 import colors from '../assets/colors/colors';
+
+const height = Dimensions.get('window').height;
 
 const Details = ({ route, navigation }) => {
   const { item } = route.params;
 
   return (
-    <View>
-      {console.log(item)}
-      <Text>{item.title}</Text>
+    <View style={styles.container}>
+      <ImageBackground source={item.imageBig} style={styles.backgroundImage}>
+        <TouchableOpacity
+          style={styles.backIcon}
+          onPress={() => navigation.goBack()}>
+          <Entypo name="chevron-left" size={32} color={colors.white} />
+        </TouchableOpacity>
+      </ImageBackground>
+      <View style={styles.descriptionWrapper} />
     </View>
   );
 };
@@ -18,6 +33,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
+  },
+  backgroundImage: {
+    height: height * 0.6,
+  },
+  backIcon: {
+    marginLeft: 20,
+    marginTop: 60,
+  },
+  descriptionWrapper: {
+    flex: 1,
+    backgroundColor: colors.white,
+    marginTop: -20,
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
   },
 });
 
